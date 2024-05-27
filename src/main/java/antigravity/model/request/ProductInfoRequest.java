81133f2e73743e474a863ced5604b08fc5d0a3ad
gahyeonkwon <zkvmflcldh0@gmail.com>
2024-05-27 18:02:43 +0900

    - controller 에서 들어오는 요청에 대한 validation 추가

--- src/main/java/antigravity/model/request/ProductInfoRequest.java
+++ src/main/java/antigravity/model/request/ProductInfoRequest.java
@@ -1,11 +1,16 @@
 package antigravity.model.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class ProductInfoRequest {
+
+    @NotBlank
     private int productId;
+
+    @NotBlank
     private int[] couponIds;
 }
