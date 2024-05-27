22a241c7d0530e1560552c7bb2becf5823597124
gahyeonkwon <zkvmflcldh0@gmail.com>
2024-05-27 18:03:32 +0900

    - 오류 코드 추가  1. request null  2. product 가격 범위 오류

--- src/main/java/antigravity/exception/code/ProductAmountErrorCode5xx.java
+++ src/main/java/antigravity/exception/code/ProductAmountErrorCode5xx.java
@@ -5,6 +5,9 @@ import org.springframework.http.HttpStatus;
 public enum ProductAmountErrorCode5xx implements ErrorCode{
 
     FINAL_PRICE_IS_MINUS(HttpStatus.INTERNAL_SERVER_ERROR, "유효하지 않은 최종 가격이 도출되었습니다."),
+
+    INVALID_REQUEST(HttpStatus.INTERNAL_SERVER_ERROR, "요청 데이터를 찾을 수 없습니다."),
+    INVALID_PRODUCT_PRICE(HttpStatus.INTERNAL_SERVER_ERROR, "제품 가격이 유효하지 않습니다."),
     CANNOT_FOUND_PRODUCT(HttpStatus.INTERNAL_SERVER_ERROR, "해당하는 상품이 없습니다."),
     CANNOT_FOUND_PROMOTION(HttpStatus.INTERNAL_SERVER_ERROR, "해당하는 프로모션이 없습니다.");
 
